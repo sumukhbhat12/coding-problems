d6696fa5bd30322854d737eda07dce95097915d9
sumukhbhat12 <96541697+sumukhbhat12@users.noreply.github.com>
2022-09-30 21:30:03 +0530

    Create populate next right pointer in each node.cpp
    There are 2 parts, the first is the populateNext and the second is the holeFiller.
    populateNext makes the left child point towards the right child recursively.
    holeFiller makes the right child point towards the sibling node's left child recursively.

---
+++ populate next right pointer in each node.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root){
+            return NULL;
+        }
+        root->next = NULL;
+        populateNext(root);
+        holeFiller(root);
+        
+        return root;
+    }
+    
+    void populateNext(Node* root){
+        if(root->left && root->right){
+            root->left->next = root->right;
+            root->right->next = NULL;
+            
+            populateNext(root->left);
+            populateNext(root->right);
+        }
+    }
+    
+    void holeFiller(Node* root){
+        if(root->left && root->right){
+            if(root->next)
+                root->right->next = root->next->left;
+            
+            holeFiller(root->left);
+            holeFiller(root->right);
+        }
+    }
+};
