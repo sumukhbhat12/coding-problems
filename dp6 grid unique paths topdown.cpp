c22e446642301097aa28b4cd3c51d9b3c1457d64
sumukhbhat12 <96541697+sumukhbhat12@users.noreply.github.com>
2022-10-27 17:48:26 +0530

    Create dp6 grid unique paths topdown.cpp
    keep a 2D dp array to save the intermediate paths taken by the robot.
    base condition is, if m and n are both zero then there is only 1 possible path to take to get to grid[0][0].
    and if either m or n are 0 then also there's only 1 possible path to that grid tile, i.e either from top or from the left.

---
+++ dp6 grid unique paths topdown.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return helper(m-1,n-1,dp);
+    }
+    
+    int helper(int m, int n, vector<vector<int>> &dp){
+        if(m == 0 && n == 0){
+            return 1;
+        }
+        if(m == 0 || n == 0){
+            return 1;
+        }
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        
+        return dp[m][n] = helper(m-1,n,dp) + helper(m,n-1,dp);
+    }
+};
