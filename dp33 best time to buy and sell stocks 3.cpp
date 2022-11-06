ee583d16901c7399cc2165b1f9321b6748a00941
sumukhbhat12 <96541697+sumukhbhat12@users.noreply.github.com>
2022-11-06 12:31:02 +0530

    Create dp33 best time to buy and sell stocks 3.cpp
    Similar to the best time to buy and sell stocks 2, but here there's a limit on the no. of transactions, that is 2.
    so we need one more variable to keep count of the no. of transactions.
    the recurrence relation is
    if b == 1 then helper(n,b,t) = max( -prices[n] + helper(n+1,!b,t), helper(n+1,b,t) ).
    else helper(n,b,t) = max( prices[n] + helper(n+1,!b,t-1), helper(n+1,b,t) ).

---
+++ dp33 best time to buy and sell stocks 3.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(3,0)));
+        //return helper2(0,1,2,prices,dp);
+        return helper3(0,1,2,prices);
+    }
+    
+    //TOPDOWN APPROACH
+    int helper(int n,int b, int t, vector<int>& prices, vector<vector<vector<int>>> &dp){
+        
+        if(t == 0)
+            return 0;
+        if(n == prices.size())
+            return 0;
+        
+        if(dp[n][b][t] != -1)
+            return dp[n][b][t];
+        
+        if(b){
+            return dp[n][b][t] = max(-prices[n] + helper(n+1,!b,t,prices,dp), helper(n+1,b,t,prices,dp));
+        }
+        else{
+            return dp[n][b][t] = max(prices[n] + helper(n+1,!b,t-1,prices,dp), helper(n+1,b,t,prices,dp));
+        }
+    }
+    
+    //BOTTOMUP APPROACH
+    int helper2(int n,int b, int t, vector<int>& prices, vector<vector<vector<int>>> &dp){
+        
+        //base cases are if i == n or k == 0 then dp[i][j][k] = 0
+        //the base case is initialized by default
+        int m = prices.size();
+        for(int i=m-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int k = 1;k<=2;k++){
+                    if(j){
+                        dp[i][j][k] = max(-prices[i] + dp[i+1][!j][k], dp[i+1][j][k]);
+                    }
+                    else{
+                        dp[i][j][k] = max(prices[i] + dp[i+1][!j][k-1], dp[i+1][j][k]);
+                    }
+                }
+            }
+        }
+        
+        return dp[n][b][t];
+    }
+    
+    //SPACE OPTIMIZATION APPROACH
+    int helper3(int n,int b, int t, vector<int>& prices){
+        
+        int m = prices.size();
+        
+        vector<vector<int>> next(2,vector<int>(3,0)), cur(2,vector<int>(3,0));
+        
+        
+        //base cases are if i == n or k == 0 then next[j][k] = 0
+        //the base case is initialized by default
+        
+        for(int i=m-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int k = 1;k<=2;k++){
+                    if(j){
+                        cur[j][k] = max(-prices[i] + next[!j][k], next[j][k]);
+                    }
+                    else{
+                        cur[j][k] = max(prices[i] + next[!j][k-1], next[j][k]);
+                    }
+                }
+            }
+            next = cur;
+        }
+        
+        return next[b][t];
+    }
+    
+};
